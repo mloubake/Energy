72b9f17a7d28ffafe8ed4d4b08afc414d49097f3
MLoub <mloubake@hotmail.com>
2018-08-09 16:20:46 -0300

    Só commit brabo

--- app/src/main/java/caiococaro/com/br/energy/MainActivity.java
+++ app/src/main/java/caiococaro/com/br/energy/MainActivity.java
@@ -2,6 +2,9 @@ package caiococaro.com.br.energy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.energyBtn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"TZZZ!!!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
